d9defb6ebd42a1500a172447a54ba1791da6b3d8
sandraherrerogomez <sandra.herrero93@hotmail.com>
2020-03-28 15:36:29 +0100

    update

--- src/main/java/com/globalcomparator/globalcomparator/GlobalcomparatorApplication.java
+++ src/main/java/com/globalcomparator/globalcomparator/GlobalcomparatorApplication.java
@@ -2,12 +2,28 @@ package com.globalcomparator.globalcomparator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
+@EnableAsync
 @SpringBootApplication
 public class GlobalcomparatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalcomparatorApplication.class, args);
+	}
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("JDAsync-");
+		executor.initialize();
+		return executor;
 	}
 
 }
--- src/main/java/com/globalcomparator/globalcomparator/service/GlobalComparatorService.java
+++ src/main/java/com/globalcomparator/globalcomparator/service/GlobalComparatorService.java
@@ -1,6 +1,7 @@
 package com.globalcomparator.globalcomparator.service;
 import com.globalcomparator.globalcomparator.model.ComparatorRequest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Service
 public class GlobalComparatorService {
     private RestTemplate restTemplate= new RestTemplateBuilder().build();
+
 
     public List<String> processComparatorRequest(ComparatorRequest request){
         List<String> data = new ArrayList<>();
@@ -23,39 +25,51 @@ public class GlobalComparatorService {
         return data;
     }
 
-    private String getCOLData(ComparatorRequest request){
+    public List<String> getAllNumbeoData(ComparatorRequest request){
+        String URL="http://localhost:8081/allData?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
+                "&country2="+request.getCountry2();
+        return this.restTemplate.getForObject(URL, List.class);
+    }
+    @Async
+    public String getCOLData(ComparatorRequest request){
         String URL="http://localhost:8081/cityComparator?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
                 "&country2="+request.getCountry2()+"&amount1="+request.getAmountCity1()+"&amount2="+request.getAmountCity2()+"&currency=EUR";
         return this.restTemplate.getForObject(URL, String.class);
     }
 
-    private String SALCalculatorData1(ComparatorRequest request){
+    @Async
+    String SALCalculatorData1(ComparatorRequest request){
         String URL="http://localhost:8082/salary?countryName="+request.getCountry1()+"&grossSalary="+request.getAmountCity1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
-    private String SALCalculatorData2(ComparatorRequest request){
+    @Async
+    String SALCalculatorData2(ComparatorRequest request){
         String URL="http://localhost:8082/salary?countryName="+request.getCountry2()+"&grossSalary="+request.getAmountCity2();
         return this.restTemplate.getForObject(URL, String.class);
 
     }
 
-    private String COLComparatorGetIndices1(ComparatorRequest request){
+    @Async
+    String COLComparatorGetIndices1(ComparatorRequest request){
         String URL="http://localhost:8081/indices?city="+request.getCity1()+"&country="+request.getCountry1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
-    private String COLComparatorGetIndices2(ComparatorRequest request){
+    @Async
+    String COLComparatorGetIndices2(ComparatorRequest request){
         String URL="http://localhost:8081/indices?city="+request.getCity2()+"&country="+request.getCountry2();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
-    private String COLComparatorGetCityPrices1(ComparatorRequest request){
+    @Async
+    String COLComparatorGetCityPrices1(ComparatorRequest request){
         String URL="http://localhost:8081/cityPrices?city="+request.getCity1()+"&country="+request.getCountry1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
-    private String COLComparatorGetCityPrices2(ComparatorRequest request){
+    @Async
+    String COLComparatorGetCityPrices2(ComparatorRequest request){
         String URL="http://localhost:8081/cityPrices?city="+request.getCity2()+"&country="+request.getCountry2();
         return this.restTemplate.getForObject(URL, String.class);
     }
