c823f56b07ec80a85e8ba0ca1813fab605f3e3f6
sandraherrerogomez <sandra.herrero93@hotmail.com>
2020-04-18 13:11:00 +0200

    deployment

--- src/main/java/com/globalcomparator/globalcomparator/service/GlobalComparatorService.java
+++ src/main/java/com/globalcomparator/globalcomparator/service/GlobalComparatorService.java
@@ -1,6 +1,7 @@
 package com.globalcomparator.globalcomparator.service;
 import com.globalcomparator.globalcomparator.model.ComparatorRequest;
 import com.globalcomparator.globalcomparator.model.NumbeoData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,11 @@ import java.util.List;
 @Service
 public class GlobalComparatorService {
     private RestTemplate restTemplate= new RestTemplateBuilder().build();
+    @Value("${colservice.host}")
+    private String colservice;
 
+    @Value("${salservice.host}")
+    private String salservice;
 
     public List<String> processComparatorRequest(ComparatorRequest request){
         List<String> data = new ArrayList<>();
@@ -37,56 +42,56 @@ public class GlobalComparatorService {
 
 
     public List<String> getAllNumbeoData(ComparatorRequest request){
-        String URL="http://localhost:8081/allData?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
+        String URL="https://"+colservice+"/allData?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
                 "&country2="+request.getCountry2();
         return this.restTemplate.getForObject(URL, List.class);
     }
 
     public NumbeoData getCOLData(ComparatorRequest request){
-        String URL="http://localhost:8081/cityComparator?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
+        String URL="https://"+colservice+"/cityComparator?cityName="+request.getCity1()+"&cityName2="+request.getCity2()+"&country1="+request.getCountry1()+
                 "&country2="+request.getCountry2()+"&amount1="+request.getAmountCity1()+"&amount2="+request.getAmountCity2()+"&currency=EUR";
         return this.restTemplate.getForObject(URL, NumbeoData.class);
     }
 
     @Async
     String SALCalculatorData1(ComparatorRequest request){
-        String URL="http://localhost:8082/salary?countryName="+request.getCountry1()+"&grossSalary="+request.getAmountCity1();
+        String URL="https://"+salservice+"/salary?countryName="+request.getCountry1()+"&grossSalary="+request.getAmountCity1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
     @Async
     String SALCalculatorData2(ComparatorRequest request){
-        String URL="http://localhost:8082/salary?countryName="+request.getCountry2()+"&grossSalary="+request.getAmountCity2();
+        String URL="https://"+salservice+"/salary?countryName="+request.getCountry2()+"&grossSalary="+request.getAmountCity2();
         return this.restTemplate.getForObject(URL, String.class);
 
     }
 
     @Async
     String COLComparatorGetIndices1(ComparatorRequest request){
-        String URL="http://localhost:8081/indices?city="+request.getCity1()+"&country="+request.getCountry1();
+        String URL="https://"+colservice+"/indices?city="+request.getCity1()+"&country="+request.getCountry1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
     @Async
     String COLComparatorGetIndices2(ComparatorRequest request){
-        String URL="http://localhost:8081/indices?city="+request.getCity2()+"&country="+request.getCountry2();
+        String URL="https://"+colservice+"/indices?city="+request.getCity2()+"&country="+request.getCountry2();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
     @Async
     String COLComparatorGetCityPrices1(ComparatorRequest request){
-        String URL="http://localhost:8081/cityPrices?city="+request.getCity1()+"&country="+request.getCountry1();
+        String URL="https://"+colservice+"/cityPrices?city="+request.getCity1()+"&country="+request.getCountry1();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
     @Async
     String COLComparatorGetCityPrices2(ComparatorRequest request){
-        String URL="http://localhost:8081/cityPrices?city="+request.getCity2()+"&country="+request.getCountry2();
+        String URL="https://"+colservice+"/cityPrices?city="+request.getCity2()+"&country="+request.getCountry2();
         return this.restTemplate.getForObject(URL, String.class);
     }
 
     public String COLComparatorListAvailableCities(){
-        String URL="http://localhost:8081/cities";
+        String URL="https://"+colservice+"/cities";
         return this.restTemplate.getForObject(URL, String.class);
     }
 }
